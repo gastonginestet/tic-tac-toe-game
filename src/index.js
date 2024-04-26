433365743848ffb71745ec80d590c3febe60a02f
Gaston Ginestet <gastonauginestet@gmail.com>
2024-04-26 12:13:36 -0300

    created first react componenct and project configured

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Rick and Morty');
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <h1>Rtest</h1>;
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
